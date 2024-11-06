9b8a984e27c513a57c43341723a6ce28693b3431
Adam Kopiec <adas073@gmail.com>
2024-11-06 21:55:53 +0100

    tmp

--- com/bartoszwalter/students/taxes/Tax.java
+++ com/bartoszwalter/students/taxes/Tax.java
@@ -8,59 +8,74 @@ import com.bartoszwalter.students.taxes.enums.TaxConstants;
 import com.bartoszwalter.students.taxes.utils.CalculationUtils;
 
 public class Tax {
-    public TaxResult calculateTax(double income, boolean isEmployment) {
-        double socialSecurity = CalculationUtils.calculatePercentage(income, EmploymentRates.SOCIAL_SECURITY.getRate());
-        double healthSecurity = CalculationUtils.calculatePercentage(income, EmploymentRates.HEALTH_SECURITY.getRate());
-        double sicknessSecurity = CalculationUtils.calculatePercentage(income,
-                EmploymentRates.SICKNESS_SECURITY.getRate());
+        public double socialSecurity;
+        public double healthSecurity;
+        public double sicknessSecurity;
 
-        double taxableIncome = isEmployment ? income : income - (socialSecurity + healthSecurity + sicknessSecurity);
+//         public TaxResult calculateTax(double income, boolean isEmployment) {
+//                 double socialSecurity = CalculationUtils.calculatePercentage(income, EmploymentRates.SOCIAL_SECURITY.getRate());
+//                 double healthSecurity = CalculationUtils.calculatePercentage(income, EmploymentRates.HEALTH_SECURITY.getRate());
+//                 double sicknessSecurity = CalculationUtils.calculatePercentage(income,
+//                         EmploymentRates.SICKNESS_SECURITY.getRate());
 
-        return calculateCommonTax(taxableIncome, socialSecurity, healthSecurity, sicknessSecurity, isEmployment);
-    }
+//                 double taxableIncome = isEmployment ? income : income - (socialSecurity + healthSecurity + sicknessSecurity);
 
-    private TaxResult calculateCommonTax(
-            double income,
-            double socialSecurity,
-            double healthSecurity,
-            double sicknessSecurity,
-            boolean isEmployment) {
+//                 return calculateCommonTax(taxableIncome, socialSecurity, healthSecurity, sicknessSecurity, isEmployment);
+//         }
 
-        double healthSocialTax9 = CalculationUtils.calculatePercentage(income, HealthSocialRates.RATE_9.getRate());
-        double healthSocialTax7_75 = CalculationUtils.calculatePercentage(income,
-                HealthSocialRates.RATE_7_75.getRate());
+        public Tax(double income) {
+                this.socialSecurity = CalculationUtils.calculatePercentage(income, EmploymentRates.SOCIAL_SECURITY.getRate());
+                this.healthSecurity = CalculationUtils.calculatePercentage(income, EmploymentRates.HEALTH_SECURITY.getRate());
+                this.sicknessSecurity = CalculationUtils.calculatePercentage(income, EmploymentRates.SICKNESS_SECURITY.getRate());
+        }
 
-        double taxDeductibleExpenses = CalculationUtils.calculatePercentage(income,
-                TaxConstants.TAX_DEDUCTIBLE_PERCENTAGE.getValue());
-        double taxedIncome = income - taxDeductibleExpenses;
+        public TaxResult calculateTax(double income) {
+                double taxableIncome = getTaxableIncome(income);
 
-        long roundedTaxedIncome = CalculationUtils.roundToNearestInt(taxedIncome);
+                return calculateCommonTax(taxableIncome);
+        }
 
-        double advanceTax = CalculationUtils.calculatePercentage(roundedTaxedIncome,
-                TaxConstants.ADVANCE_TAX_RATE.getValue());
-        double taxFreeIncome = TaxConstants.TAX_FREE_ALLOWANCE.getValue();
+        private double getTaxableIncome(double income) {
+                return 0.0;
+        }
 
-        double advanceTaxPaid = advanceTax - healthSocialTax7_75 - taxFreeIncome;
-        long roundedAdvanceTaxPaid = CalculationUtils.roundToNearestInt(advanceTaxPaid);
+        private TaxResult calculateCommonTax(double income) {
 
-        double costs = socialSecurity + healthSecurity + sicknessSecurity + healthSocialTax9 + roundedAdvanceTaxPaid;
-        double netIncome = income - costs;
+                double healthSocialTax9 = CalculationUtils.calculatePercentage(income, HealthSocialRates.RATE_9.getRate());
+                double healthSocialTax7_75 = CalculationUtils.calculatePercentage(income,
+                        HealthSocialRates.RATE_7_75.getRate());
 
-        return new TaxResultBuilder()
-                .setIncome(income)
-                .setSocialSecurity(socialSecurity)
-                .setHealthSecurity(healthSecurity)
-                .setSicknessSecurity(sicknessSecurity)
-                .setHealthSocialTax9(healthSocialTax9)
-                .setHealthSocialTax7_75(healthSocialTax7_75)
-                .setTaxDeductibleExpenses(taxDeductibleExpenses)
-                .setTaxedIncome(taxedIncome)
-                .setRoundedTaxedIncome(roundedTaxedIncome)
-                .setAdvanceTax(advanceTax)
-                .setTaxFreeIncome(taxFreeIncome)
-                .setAdvanceTaxPaid(advanceTaxPaid)
-                .setRoundedAdvanceTaxPaid(roundedAdvanceTaxPaid)
-                .setNetIncome(netIncome)
-                .build();
-    }
+                double taxDeductibleExpenses = CalculationUtils.calculatePercentage(income,
+                        TaxConstants.TAX_DEDUCTIBLE_PERCENTAGE.getValue());
+                double taxedIncome = income - taxDeductibleExpenses;
+
+                long roundedTaxedIncome = CalculationUtils.roundToNearestInt(taxedIncome);
+
+                double advanceTax = CalculationUtils.calculatePercentage(roundedTaxedIncome,
+                        TaxConstants.ADVANCE_TAX_RATE.getValue());
+                double taxFreeIncome = TaxConstants.TAX_FREE_ALLOWANCE.getValue();
+
+                double advanceTaxPaid = advanceTax - healthSocialTax7_75 - taxFreeIncome;
+                long roundedAdvanceTaxPaid = CalculationUtils.roundToNearestInt(advanceTaxPaid);
+
+                double costs = this.socialSecurity + this.healthSecurity + this.sicknessSecurity + healthSocialTax9 + roundedAdvanceTaxPaid;
+                double netIncome = income - costs;
+
+                return new TaxResultBuilder()
+                        .setIncome(income)
+                        .setSocialSecurity(this.socialSecurity)
+                        .setHealthSecurity(this.healthSecurity)
+                        .setSicknessSecurity(sicknessSecurity)
+                        .setHealthSocialTax9(healthSocialTax9)
+                        .setHealthSocialTax7_75(healthSocialTax7_75)
+                        .setTaxDeductibleExpenses(taxDeductibleExpenses)
+                        .setTaxedIncome(taxedIncome)
+                        .setRoundedTaxedIncome(roundedTaxedIncome)
+                        .setAdvanceTax(advanceTax)
+                        .setTaxFreeIncome(taxFreeIncome)
+                        .setAdvanceTaxPaid(advanceTaxPaid)
+                        .setRoundedAdvanceTaxPaid(roundedAdvanceTaxPaid)
+                        .setNetIncome(netIncome)
+                        .build();
+        }
 }
---
+++ com/bartoszwalter/students/taxes/models/CivilContractTax.java
@@ -0,0 +1,3 @@
+public class CivilContractTax extends Tax {
+	
+}
